41c11926ffbadbe2dd3352cbef774c31a948f980
Paul Bagley <pbagley77@gmail.com>
2018-07-10 15:42:11 -0500

    add items filter by card type integrated

---
+++ app/src/main/java/com/a5402technologies/shadowsofbrimstonecompanion/Activities/Main/Inventory/AddItems/ChooseSetActivity.java
@@ -0,0 +1,13 @@
+package com.a5402technologies.shadowsofbrimstonecompanion.Activities.Main.Inventory.AddItems;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+
+public class ChooseSetActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_choose_set);
+    }
+}
